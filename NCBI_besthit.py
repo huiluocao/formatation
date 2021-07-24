bbaf13a4509a5e4e8bffa58ec5c902500a0ce9cf
Huiluo Cao <huiluocao@gmail.com>
2021-07-24 11:04:56 +0800

    Update NCBI_besthit.py

--- NCBI_besthit.py
+++ NCBI_besthit.py
@@ -5,7 +5,7 @@ blast_df_fil=pd.DataFrame()
 for file in os.listdir('./'):
     if file.endswith('csv'):
         blast_df = pd.read_table(file,sep = ',', engine = 'python', header = None)
-        #blast_df_fil = blast_df[blast_df.groupby([0])[2,3,11].transform(max)==blast_df[2,3,11]]
+        #blast_df_fil = blast_df[blast_df.groupby([0])[11].transform(max)==blast_df[11]]
         blast_df_fil=blast_df_fil.append(blast_df.groupby([0]).first().reset_index())
 print(blast_df_fil)
 with open('../16S_batch2_ncbi_besthit.txt','w') as f:
